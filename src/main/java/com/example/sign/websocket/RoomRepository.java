dd8d753fae61b7c77c3a2225f71fdb98ce5dc469
cypsyco <jyh0872@gmail.com>
2024-01-25 04:56:15 +0900

    feat: Room Repository 수정

--- src/main/java/com/example/sign/websocket/RoomRepository.java
+++ src/main/java/com/example/sign/websocket/RoomRepository.java
@@ -2,6 +2,7 @@ package com.example.sign.websocket;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends JpaRepository<ChatRoom, String> {
+public interface RoomRepository extends JpaRepository<Room, String> {
 }
+
 
